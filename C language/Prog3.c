3d25e9f2c52449651c77cc6cc3e248c7b6b106c7
manmiyani154 <manmiyani.it24d2@scet.ac.in>
2025-07-17 20:25:21 +0530

    Update Prog3.c

--- C language/Prog3.c
+++ C language/Prog3.c
@@ -7,14 +7,14 @@ int main()
         printf("Enter %d element:", i + 1);
         scanf("%d", &arr[i]);
     }
-    for (int i = 0; i < 10; i++) printf("\nThe address of arr[%d] using &: %X",i+1,&arr[i]);
+    for (int i = 0; i < 10; i++) printf("\nThe address of arr[%d] using &: %X",i,&arr[i]);
     
     printf("\n----------");
     printf("\nThe Base address for arr[0]: %X",&arr[0]);
-    for (int i = 1; i < 9; i++) printf("\nThe address of arr[%d] using Base Address: %X",i,&arr[0 + i]);
+    for (int i = 1; i < 10; i++) printf("\nThe address of arr[%d] using Base Address: %X",i,&arr[0 + i]);
     
     printf("\n----------");
     printf("\nThe Base address for arr[0]: %X",&arr[0]);
-    for (int i = 1; i < 9; i++) printf("\nThe address of arr[%d] using formula: %X",i,&arr[0 + i] + sizeof(int));
+    for (int i = 1; i < 10; i++) printf("\nThe address of arr[%d] using formula: %X",i,&arr[0 + i] + sizeof(int));
     return 0;
 }
