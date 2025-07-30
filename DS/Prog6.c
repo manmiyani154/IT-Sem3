aadc78fc1c126fcf8d5e45dd87f838a79c0182d0
manmiyani154 <manmiyani.it24d2@scet.ac.in>
2025-07-30 22:49:39 +0530

    Update Prog6.c

--- DS/Prog6.c
+++ DS/Prog6.c
@@ -9,92 +9,146 @@ char stack[SIZE];
 int top = -1;
 
 void push(char val) {
+    if (top >= SIZE - 1) {
+        printf("Stack Overflow! Cannot push '%c'\n", val);
+        exit(1);
+    }
     stack[++top] = val;
 }
+
 char pop() {
+    if (top == -1) {
+        printf("Stack Underflow! Cannot pop\n");
+        exit(1);
+    }
     return stack[top--];
 }
+
 char peek() {
+    if (top == -1) {
+        printf("Stack is Empty! Cannot peek\n");
+        exit(1);
+    }
     return stack[top];
 }
 
 int f(char symbol) {
-    switch(symbol) {
-        case '+': case '-': return 1;
-        case '*': case '/': return 3;
+    switch (symbol) {
+        case '+':
+        case '-': return 1;
+        case '*':
+        case '/': return 3;
         case '^': return 6;
         case '(': return 9;
         case ')': return 0;
-        default: return 7;
+        default : return 7;
     }
 }
 
 int g(char symbol) {
-    switch(symbol) {
-        case '+': case '-': return 2;
-        case '*': case '/': return 4;
+    switch (symbol) {
+        case '+':
+        case '-': return 2;
+        case '*':
+        case '/': return 4;
         case '^': return 5;
         case '(': return 0;
-        default: return 8;
+        default : return 8;
     }
 }
 
 int R(char symbol) {
-    if (isalnum(symbol))
-        return 1;
-    else
-        return -1;
+    return isalnum(symbol) ? 1 : -1;
 }
 
-void infixToPostfix(char* infix) {
-    char postfix[SIZE];
+char postfix[SIZE];
+
+void infixToPostfix(char *infix) {
     int i = 0, j = 0, rank = 0;
     char temp;
-
     push('(');
     strcat(infix, ")");
-
-    while(infix[i] != '\0') {
+    while (infix[i] != '\0') {
         char sym = infix[i];
-
         if (sym == ' ') {
             i++;
             continue;
         }
-
         while (g(peek()) > f(sym)) {
             temp = pop();
             postfix[j++] = temp;
             rank += R(temp);
-
             if (rank < 1) {
                 printf("Invalid Expression\n");
-                return;
+                exit(1);
             }
         }
-
-        if (g(peek()) != f(sym)) {
-            push(sym);
-        } else {
-            pop();
-        }
-
+        if (g(peek()) != f(sym)) push(sym);
+        else pop();
         i++;
     }
-
     postfix[j] = '\0';
-
     if (top != -1 || rank != 1) {
         printf("Invalid Expression\n");
+        exit(1);
     } else {
         printf("Postfix: %s\n", postfix);
     }
 }
 
+void evaluatePostfix(char *postfix) {
+    float evalStack[SIZE];
+    int evalTop = -1;
+    float variables[26] = {0};
+    int used[26] = {0};
+    for (int i = 0; postfix[i] != '\0'; i++) {
+        char sym = postfix[i];
+        if (isalpha(sym)) {
+            int index = sym - 'a';
+            if (!used[index]) {
+                printf("Enter value of %c: ", sym);
+                scanf("%f", &variables[index]);
+                used[index] = 1;
+            }
+            evalStack[++evalTop] = variables[index];
+        } else {
+            if (evalTop < 1) {
+                printf("Invalid Expression for Evaluation\n");
+                return;
+            }
+            float b = evalStack[evalTop--];
+            float a = evalStack[evalTop--];
+            float res;
+            switch (sym) {
+                case '+': res = a + b; break;
+                case '-': res = a - b; break;
+                case '*': res = a * b; break;
+                case '/':
+                    if (b == 0) {
+                        printf("Division by zero error!\n");
+                        return;
+                    }
+                    res = a / b;
+                    break;
+                default:
+                    printf("Unknown operator '%c'\n", sym);
+                    return;
+            }
+            evalStack[++evalTop] = res;
+        }
+    }
+    if (evalTop == 0) {
+        printf("Result = %.2f\n", evalStack[evalTop]);
+    } else {
+        printf("Invalid postfix evaluation\n");
+    }
+}
+
 int main() {
     char infix[SIZE];
-    printf("Enter infix expression (spaces allowed): ");
+    printf("Enter infix expression (with spaces if needed): ");
     scanf(" %[^\n]", infix);
     infixToPostfix(infix);
+    evaluatePostfix(postfix);
     return 0;
 }
