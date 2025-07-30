27b865a24c34b4fcc746d9de53a1171d4fe7e947
manmiyani154 <manmiyani.it24d2@scet.ac.in>
2025-07-30 22:09:53 +0530

    Update Prog6.c

--- DS/Prog6.c
+++ DS/Prog6.c
@@ -1,53 +1,100 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 
-char stack[20];
+#define SIZE 100
+
+char stack[SIZE];
 int top = -1;
 
-void push(char ch) {
-    stack[++top] = ch;
+void push(char val) {
+    stack[++top] = val;
 }
-
 char pop() {
     return stack[top--];
 }
-
-int precedence(char ch) {
-    if (ch == '^') return 3;
-    if (ch == '*' || ch == '/') return 2;
-    if (ch == '+' || ch == '-') return 1;
-    return 0;
+char peek() {
+    return stack[top];
 }
 
-int isOperator(char ch) {
-    return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^';
+int f(char symbol) {
+    switch(symbol) {
+        case '+': case '-': return 1;
+        case '*': case '/': return 3;
+        case '^': return 6;
+        case '(': return 9;
+        case ')': return 0;
+        default: return 7;
+    }
+}
+
+int g(char symbol) {
+    switch(symbol) {
+        case '+': case '-': return 2;
+        case '*': case '/': return 4;
+        case '^': return 5;
+        case '(': return 0;
+        default: return 8;
+    }
+}
+
+int R(char symbol) {
+    if (isalnum(symbol))
+        return 1;
+    else
+        return -1;
+}
+
+void infixToPostfix(char* infix) {
+    char postfix[SIZE];
+    int i = 0, j = 0, rank = 0;
+    char temp;
+
+    push('(');
+    strcat(infix, ")");
+
+    while(infix[i] != '\0') {
+        char sym = infix[i];
+
+        if (sym == ' ') {
+            i++;
+            continue;
+        }
+
+        while (g(peek()) > f(sym)) {
+            temp = pop();
+            postfix[j++] = temp;
+            rank += R(temp);
+
+            if (rank < 1) {
+                printf("Invalid Expression\n");
+                return;
+            }
+        }
+
+        if (g(peek()) != f(sym)) {
+            push(sym);
+        } else {
+            pop();
+        }
+
+        i++;
+    }
+
+    postfix[j] = '\0';
+
+    if (top != -1 || rank != 1) {
+        printf("Invalid Expression\n");
+    } else {
+        printf("Postfix: %s\n", postfix);
+    }
 }
 
 int main() {
-
-    char infix[100], postfix[100];
-    int i = 0, j = 0;
-    scanf("%s", infix);
-    while (infix[i] != '\0') {
-        char ch = infix[i];
-        if (isalnum(ch)) {
-            postfix[j++] = ch;
-        } else if (ch == '(') {
-            push(ch);
-        } else if (ch == ')') {
-            while (top != -1 && stack[top] != '(')
-                postfix[j++] = pop();
-            pop();
-        } else {
-            while (top != -1 && precedence(stack[top]) >= precedence(ch))
-                postfix[j++] = pop();
-            push(ch);
-        }
-        i++;
-    }
-    while (top != -1)
-        postfix[j++] = pop();
-    postfix[j] = '\0';
-    printf("%s\n", postfix);
+    char infix[SIZE];
+    printf("Enter infix expression (spaces allowed): ");
+    scanf(" %[^\n]", infix);
+    infixToPostfix(infix);
     return 0;
 }
