5e405b47f96141cc5f41a418fd25c975000f1451
manmiyani154 <manmiyani.it24d2@scet.ac.in>
2025-08-01 02:24:53 +0530

    convert and evalute.c

--- DS/Prog7.c
+++ DS/Prog7.c
@@ -1,36 +1,165 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 
-int stack[20], top = -1;
+#define SIZE 100
 
-void push(int val) {
+char stack[SIZE];
+int top = -1;
+char postfix[SIZE];
+
+// Stack operations
+void push(char val) {
+    if (top >= SIZE - 1) {
+        printf("Stack Overflow! Cannot push '%c'\n", val);
+        exit(1);
+    }
     stack[++top] = val;
 }
 
-int pop() {
+char pop() {
+    if (top == -1) {
+        printf("Stack Underflow! Cannot pop\n");
+        exit(1);
+    }
     return stack[top--];
 }
 
-int main() {
+char peek() {
+    if (top == -1) {
+        printf("Stack is Empty! Cannot peek\n");
+        exit(1);
+    }
+    return stack[top];
+}
 
-    char exp[100];
-    int i = 0, op1, op2;
-    scanf("%s", exp);
-    while (exp[i] != '\0') {
-        if (isdigit(exp[i])) {
-            push(exp[i] - '0');
-        } else {
-            op2 = pop();
-            op1 = pop();
-            switch (exp[i]) {
-                case '+': push(op1 + op2); break;
-                case '-': push(op1 - op2); break;
-                case '*': push(op1 * op2); break;
-                case '/': push(op1 / op2); break;
+// Incoming precedence
+int f(char sym) {
+    switch (sym) {
+        case '+':
+        case '-': return 1;
+        case '*':
+        case '/': return 3;
+        case '^': return 6;
+        case '(': return 9;
+        case ')': return 0;
+        default : return 7;
+    }
+}
+
+// In-stack precedence
+int g(char sym) {
+    switch (sym) {
+        case '+':
+        case '-': return 2;
+        case '*':
+        case '/': return 4;
+        case '^': return 5;
+        case '(': return 0;
+        default : return 8;
+    }
+}
+
+// Rank: 1 for operands, -1 for operators
+int R(char sym) {
+    return isalnum(sym) ? 1 : -1;
+}
+
+// Convert infix to postfix
+void infixToPostfix(char *infix) {
+    int i = 0, j = 0, rank = 0;
+    char temp;
+    push('(');
+    strcat(infix, ")");
+
+    while (infix[i] != '\0') {
+        char sym = infix[i];
+        if (sym == ' ') {
+            i++;
+            continue;
+        }
+        while (g(peek()) > f(sym)) {
+            temp = pop();
+            postfix[j++] = temp;
+            rank += R(temp);
+            if (rank < 1) {
+                printf("Invalid Expression\n");
+                exit(1);
             }
         }
+        if (g(peek()) != f(sym)) push(sym);
+        else pop();
         i++;
     }
-    printf("%d\n", pop());
+
+    postfix[j] = '\0';
+
+    if (top != -1 || rank != 1) {
+        printf("Invalid Expression\n");
+        exit(1);
+    } else {
+        printf("Postfix: %s\n", postfix);
+    }
+}
+
+// Evaluate postfix expression
+void evaluatePostfix(char *postfix) {
+    float evalStack[SIZE];
+    int evalTop = -1;
+    float variables[26] = {0}; // For a-z
+    int used[26] = {0};        // Track if value already entered
+
+    for (int i = 0; postfix[i] != '\0'; i++) {
+        char sym = postfix[i];
+
+        if (isalpha(sym)) {
+            int index = sym - 'a';
+            if (!used[index]) {
+                printf("Enter value of %c: ", sym);
+                scanf("%f", &variables[index]);
+                used[index] = 1;
+            }
+            evalStack[++evalTop] = variables[index];
+        } else {
+            if (evalTop < 1) {
+                printf("Invalid Expression for Evaluation\n");
+                return;
+            }
+            float b = evalStack[evalTop--];
+            float a = evalStack[evalTop--];
+            float res;
+            switch (sym) {
+                case '+': res = a + b; break;
+                case '-': res = a - b; break;
+                case '*': res = a * b; break;
+                case '/':
+                    if (b == 0) {
+                        printf("Division by zero error!\n");
+                        return;
+                    }
+                    res = a / b;
+                    break;
+                default:
+                    printf("Unknown operator '%c'\n", sym);
+                    return;
+            }
+            evalStack[++evalTop] = res;
+        }
+    }
+
+    if (evalTop == 0) {
+        printf("Result = %.2f\n", evalStack[evalTop]);
+    } else {
+        printf("Invalid postfix evaluation\n");
+    }
+}
+
+int main() {
+    char infix[SIZE];
+    printf("Enter infix expression: ");
+    scanf(" %[^\n]", infix);
+    infixToPostfix(infix);
+    evaluatePostfix(postfix);
     return 0;
 }
