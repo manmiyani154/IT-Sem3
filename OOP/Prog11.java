a102893cd81da9c7296d3214a7cadd32df220a60
manmiyani154 <manmiyani.it24d2@scet.ac.in>
2025-09-26 08:28:42 +0530

    Update Prog11.java

--- OOP/Prog11.java
+++ OOP/Prog11.java
@@ -1,28 +1,79 @@
 class Player {
-    String name = "Player";
+String name;
+int age;
+
+
+Player(String name, int age) {
+this.name = name;
+this.age = age;
 }
+
+
+void show() {
+System.out.println("Name: " + name + ", Age: " + age);
+}
+}
+
 
 class CricketPlayer extends Player {
-    String team = "Cricket Team";
+String team;
+
+
+CricketPlayer(String name, int age, String team) {
+super(name, age);
+this.team = team;
 }
+
+
+void show() {
+super.show();
+System.out.println("Team: " + team);
+}
+}
+
 
 class Batsman extends CricketPlayer {
-    void show() {
-        System.out.println(name + " - " + team + " - Batsman");
-    }
+int runs;
+
+
+Batsman(String name, int age, String team, int runs) {
+super(name, age, team);
+this.runs = runs;
 }
+
+
+void show() {
+super.show();
+System.out.println("Runs: " + runs);
+}
+}
+
 
 class Bowler extends CricketPlayer {
-    void show() {
-        System.out.println(name + " - " + team + " - Bowler");
-    }
+int wickets;
+
+
+Bowler(String name, int age, String team, int wickets) {
+super(name, age, team);
+this.wickets = wickets;
 }
 
-public class Prog11 {
-    public static void main(String[] args) {
-        Batsman b = new Batsman();
-        Bowler w = new Bowler();
-        b.show();
-        w.show();
-    }
+
+void show() {
+super.show();
+System.out.println("Wickets: " + wickets);
+}
+}
+
+
+class Prog11 {
+public static void main(String[] args) {
+Batsman b = new Batsman("Virat", 34, "India", 12000);
+Bowler bw = new Bowler("Bumrah", 30, "India", 250);
+
+
+System.out.println("\nBatsman Details:");
+b.show();
+System.out.println("\nBowler Details:");
+bw.show();
 }
