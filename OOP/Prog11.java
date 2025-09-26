879f55ce1e20a58588bacd3ae9c8b4f485192514
manmiyani154 <manmiyani.it24d2@scet.ac.in>
2025-09-26 08:29:32 +0530

    Update Prog11.java

--- OOP/Prog11.java
+++ OOP/Prog11.java
@@ -1,70 +1,58 @@
 class Player {
-String name;
-int age;
+    String name;
+    int age;
 
+    Player(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
-Player(String name, int age) {
-this.name = name;
-this.age = age;
+    void show() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
 }
-
-
-void show() {
-System.out.println("Name: " + name + ", Age: " + age);
-}
-}
-
 
 class CricketPlayer extends Player {
-String team;
+    String team;
 
+    CricketPlayer(String name, int age, String team) {
+        super(name, age);
+        this.team = team;
+    }
 
-CricketPlayer(String name, int age, String team) {
-super(name, age);
-this.team = team;
+    void show() {
+        super.show();
+        System.out.println("Team: " + team);
+    }
 }
-
-
-void show() {
-super.show();
-System.out.println("Team: " + team);
-}
-}
-
 
 class Batsman extends CricketPlayer {
-int runs;
+    int runs;
 
+    Batsman(String name, int age, String team, int runs) {
+        super(name, age, team);
+        this.runs = runs;
+    }
 
-Batsman(String name, int age, String team, int runs) {
-super(name, age, team);
-this.runs = runs;
+    void show() {
+        super.show();
+        System.out.println("Runs: " + runs);
+    }
 }
-
-
-void show() {
-super.show();
-System.out.println("Runs: " + runs);
-}
-}
-
 
 class Bowler extends CricketPlayer {
-int wickets;
+    int wickets;
 
+    Bowler(String name, int age, String team, int wickets) {
+        super(name, age, team);
+        this.wickets = wickets;
+    }
 
-Bowler(String name, int age, String team, int wickets) {
-super(name, age, team);
-this.wickets = wickets;
+    void show() {
+        super.show();
+        System.out.println("Wickets: " + wickets);
+    }
 }
-
-
-void show() {
-super.show();
-System.out.println("Wickets: " + wickets);
-}
-}
-
 
 class Prog11 {
 public static void main(String[] args) {
