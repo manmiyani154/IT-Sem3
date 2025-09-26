8a3f1f8a806de3c9d6658d8b62a63c445d4308d3
manmiyani154 <manmiyani.it24d2@scet.ac.in>
2025-09-26 08:31:13 +0530

    Update Prog13.java

--- OOP/Prog13.java
+++ OOP/Prog13.java
@@ -1,35 +1,42 @@
-abstract class Shape {
-    double a, b;
-    Shape(double x, double y) {
-        a = x;
-        b = y;
+abstract class Shape2 {
+    double dim1, dim2;
+
+    Shape2(double d1, double d2) {
+        dim1 = d1;
+        dim2 = d2;
     }
+
     abstract double area();
 }
 
-class Rectangle extends Shape {
-    Rectangle(double x, double y) {
-        super(x, y);
+class Rectangle2 extends Shape2 {
+    Rectangle2(double d1, double d2) {
+        super(d1, d2);
     }
+
     double area() {
-        return a * b;
+        return dim1 * dim2;
     }
 }
 
-class Triangle extends Shape {
-    Triangle(double x, double y) {
-        super(x, y);
+class Triangle2 extends Shape2 {
+    Triangle2(double d1, double d2) {
+        super(d1, d2);
     }
+
     double area() {
-        return 0.5 * a * b;
+        return 0.5 * dim1 * dim2;
     }
 }
 
-public class Prog13 {
+class Prog13 {
     public static void main(String[] args) {
-        Shape s1 = new Rectangle(4, 5);
-        Shape s2 = new Triangle(3, 6);
-        System.out.println("Rectangle area: " + s1.area());
-        System.out.println("Triangle area: " + s2.area());
+        Shape2 s;
+
+        s = new Rectangle2(5, 10);
+        System.out.println("Area of Rectangle: " + s.area());
+
+        s = new Triangle2(6, 8);
+        System.out.println("Area of Triangle: " + s.area());
     }
 }
