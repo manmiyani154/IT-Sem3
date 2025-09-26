b279d43d47db1c779b766d0ad974e8021cd4862c
manmiyani154 <manmiyani.it24d2@scet.ac.in>
2025-09-26 08:30:09 +0530

    Update Prog12.java

--- OOP/Prog12.java
+++ OOP/Prog12.java
@@ -1,9 +1,9 @@
 class Shape {
-    double a, b;
+    double dim1, dim2;
 
-    Shape(double x, double y) {
-        a = x;
-        b = y;
+    Shape(double d1, double d2) {
+        dim1 = d1;
+        dim2 = d2;
     }
 
     double area() {
@@ -12,30 +12,31 @@ class Shape {
 }
 
 class Rectangle extends Shape {
-    Rectangle(double x, double y) {
-        super(x, y);
+    Rectangle(double d1, double d2) {
+        super(d1, d2);
     }
 
     double area() {
-        return a * b;
+        return super.dim1 * super.dim2;
     }
 }
 
 class Triangle extends Shape {
-    Triangle(double x, double y) {
-        super(x, y);
+    Triangle(double d1, double d2) {
+        super(d1, d2);
     }
 
     double area() {
-        return 0.5 * a * b;
+        return 0.5 * super.dim1 * super.dim2;
     }
 }
 
-public class Prog12 {
-    public static void main(String[] args) {
-        Rectangle r = new Rectangle(4, 5);
-        Triangle t = new Triangle(3, 6);
-        System.out.println("Rectangle area: " + r.area());
-        System.out.println("Triangle area: " + t.area());
-    }
+class Prog12 {
+public static void main(String[] args) {
+Rectangle r = new Rectangle(10, 5);
+Triangle t = new Triangle(8, 6);
+
+
+System.out.println("Area of Rectangle: " + r.area());
+System.out.println("Area of Triangle: " + t.area());
 }
